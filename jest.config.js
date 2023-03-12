2ea34dc2e48fa6cd9fc6df8e2203eedd6d886be1
MariaG <MGrigorenko@bellintegrator.com>
2023-03-12 17:26:58 +0300

    all chages in one commit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const config = {
+    verbose: true,
+    collectCoverageFrom: [
+      "**/*.{js,jsx}",
+      "!**/node_modules/**",
+      "!**/coverage/**",
+      "!jest.config.js",
+    ],
+    coverageThreshold: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+    },
+  };
+  
+  module.exports = config;
